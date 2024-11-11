cc04bea8a8f97376ed44e741bdea482cb75e334c
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2024-11-11 17:26:58 +0100

    ajout de HopitalFantastique.java

---
+++ src/main/java/HopitalsFantastiques/HopitalFantastique.java
@@ -0,0 +1,71 @@
+package HopitalsFantastiques;
+
+import java.util.ArrayList;
+import java.util.List;
+import Medecins.Medecin;
+import ServicesMedicaux.ServiceMedical;
+import Creatures.Creature;
+
+public class HopitalFantastique {
+    private String nom;
+    private int SERVICES_MAX;
+    private List<ServiceMedical> servicesMedicaux;
+    private List<Medecin> medecins;
+
+    public HopitalFantastique(String nom) {
+        this.nom = nom;
+        this.SERVICES_MAX = 5;
+        this.servicesMedicaux = new ArrayList<>();
+        this.medecins = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void ajouterService(ServiceMedical service) {
+        if (servicesMedicaux.size() < SERVICES_MAX) {
+            servicesMedicaux.add(service);
+            System.out.println("Service " + service.getNom() + " a été ajouté à l'hôpital " + nom);
+        } else {
+            System.out.println("Nombre maximal de services atteint dans l'hôpital " + nom);
+        }
+    }
+
+    public void ajouterMedecin(Medecin medecin) {
+        medecins.add(medecin);
+        System.out.println("Médecin " + medecin.getNom() + " a été ajouté à l'hôpital " + nom);
+    }
+
+    public int nombreTotalCreatures() {
+        int total = 0;
+        for (ServiceMedical service : servicesMedicaux) {
+            total += service.getCreatures().size();
+        }
+        return total;
+    }
+
+    public void afficherCreaturesDansServices() {
+        System.out.println("Créatures présentes dans les services de l'hôpital " + nom + " :");
+        for (ServiceMedical service : servicesMedicaux) {
+            System.out.println("Service " + service.getNom() + " :");
+            for (Creature creature : service.getCreatures()) {
+                System.out.println(" - " + creature.getNom());
+            }
+        }
+    }
+
+    public void afficherCaracteristiques() {
+        System.out.println("Hôpital Fantastique : " + nom);
+        System.out.println("Nombre maximal de services : " + SERVICES_MAX);
+        System.out.println("Services médicaux :");
+        for (ServiceMedical service : servicesMedicaux) {
+            service.afficherCaracteristiques();
+        }
+        System.out.println("Médecins :");
+        for (Medecin medecin : medecins) {
+            System.out.println(" - " + medecin.getNom() + ", Sexe : " + medecin.getSexe() + ", Âge : " + medecin.getAge());
+        }
+        System.out.println("Nombre total de créatures dans l'hôpital : " + nombreTotalCreatures());
+    }
+}
